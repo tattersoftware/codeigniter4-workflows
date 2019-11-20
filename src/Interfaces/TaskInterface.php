6f0921cef2d19bc1b9547b6ff370485cb4d51307
Matt Gatner <mgatner@tattersoftware.com>
2019-11-20 18:29:16 +0000

    Update interface for removed task methods

--- src/Interfaces/TaskInterface.php
+++ src/Interfaces/TaskInterface.php
@@ -1,27 +1,22 @@
 <?php namespace Tatter\Workflows\Interfaces;
 
-use Myth\Auth\Entities\User;
-
 interface TaskInterface
-{	
-	// provided by TasksTrait
-	public function __construct();
-	
-	// magic wrapper for getting values from the definition
-	// provided by TasksTrait
+{		
+	// Magic wrapper for getting values from the definition
+	// (provided by TasksTrait)
     public function __get(string $name);
 	
-	// create the database record of this task based on its definition
-	// provided by TasksTrait
+	// Create the database record of this task based on its definition
+	// (provided by TasksTrait)
 	public function register();
 	
-	// soft delete this task from the database
-	// provided by TasksTrait
+	// Soft delete this task from the database
+	// (provided by TasksTrait)
 	public function remove();
 	
-	// run when a job progresses forward through the workflow
+	// Run when a job progresses forward through the workflow
 	public function up();
 	
-	// run when job regresses back through the workflow
+	// Run when job regresses back through the workflow
 	public function down();
 }
