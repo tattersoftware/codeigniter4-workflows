b4f07b08f4df981d75ba5ba386a5cb179f0ca321
Matt Gatner <matt.gatner@duke.edu>
2020-06-24 20:14:23 -0400

    Move task registration to dedicated class

--- src/Commands/TasksRegister.php
+++ src/Commands/TasksRegister.php
@@ -3,7 +3,7 @@
 use CodeIgniter\CLI\BaseCommand;
 use CodeIgniter\CLI\CLI;
 use Config\Services;
-use Tatter\Workflows\Models\TaskModel;
+use Tatter\Workflows\Registrar;
 
 class TasksRegister extends BaseCommand
 {
@@ -16,64 +16,9 @@ class TasksRegister extends BaseCommand
 
 	public function run(array $params = [])
     {
-		$tasks = new TaskModel();
-		$locator = Services::locator(true);
+		$count = Registrar::tasks();
 
-		// Get all namespaces from the autoloader
-		$namespaces = Services::autoloader()->getNamespace();
-		
-		// Scan each namespace for tasks
-		$flag = false;
-		foreach ($namespaces as $namespace => $paths)
-		{
-			// Get any files in Tasks/ for this namespace
-			$files = $locator->listNamespaceFiles($namespace, '/Tasks/');
-			
-			foreach ($files as $file)
-			{
-				// Skip non-PHP files
-				if (substr($file, -4) !== '.php')
-				{
-					continue;
-				}
-				
-				// Get namespaced class name
-				$name = basename($file, '.php');
-				$class = $namespace . '\Tasks\\' . $name;
-				
-				include_once $file;
-
-				// Validate the class
-				if (! class_exists($class, false))
-				{
-					throw new \RuntimeException("Could not locate {$class} in {$file}");
-				}
-				$instance = new $class();
-				
-				// Validate the method
-				if (! is_callable([$instance, 'register']))
-				{
-					throw new \RuntimeException("Missing 'register' method for {$class} in {$file}");
-				}
-				
-				// Register it
-				$result = $instance->register();
-				
-				// If this was a new registration, add the namespaced class
-				if (is_int($result))
-				{
-					$flag = true;
-					
-					$task = $tasks->find($result);
-					$task->class = $class;
-					$tasks->save($task);
-				
-					CLI::write("Registered {$task->name} from {$class}", 'green');
-				}
-			}
-		}
-		
-		if ($flag == false)
+		if ($count === 0)
 		{
 			CLI::write('No new tasks found in any namespace.', 'yellow');
 			return;
---
+++ src/Registrar.php
@@ -0,0 +1,81 @@
+<?php namespace Tatter\Workflows;
+
+use CodeIgniter\CLI\CLI;
+use Config\Services;
+use Tatter\Workflows\Models\TaskModel;
+
+/**
+ * Class to handle task registration.
+ */
+class Registrar
+{
+	/**
+	 * Scan all namespaces for new tasks to laod into the database
+	 *
+	 * @return int  Number of new tasks registered
+	 */
+	static public function tasks(): int
+    {
+		$tasks   = model(TaskModel::class);
+		$locator = Services::locator(true);
+
+		// Get all namespaces from the autoloader
+		$namespaces = Services::autoloader()->getNamespace();
+		
+		// Scan each namespace for tasks
+		$count = 0;
+		foreach ($namespaces as $namespace => $paths)
+		{
+			// Get any files in Tasks/ for this namespace
+			$files = $locator->listNamespaceFiles($namespace, '/Tasks/');
+			
+			foreach ($files as $file)
+			{
+				// Skip non-PHP files
+				if (substr($file, -4) !== '.php')
+				{
+					continue;
+				}
+				
+				// Get namespaced class name
+				$name  = basename($file, '.php');
+				$class = $namespace . '\Tasks\\' . $name;
+				
+				include_once $file;
+
+				// Validate the class
+				if (! class_exists($class, false))
+				{
+					throw new \RuntimeException("Could not locate {$class} in {$file}");
+				}
+				$instance = new $class();
+				
+				// Validate the method
+				if (! is_callable([$instance, 'register']))
+				{
+					throw new \RuntimeException("Missing 'register' method for {$class} in {$file}");
+				}
+				
+				// Register it
+				$result = $instance->register();
+				
+				// If this was a new registration, add the namespaced class
+				if (is_int($result))
+				{
+					$count++;
+					
+					$task = $tasks->find($result);
+					$task->class = $class;
+					$tasks->save($task);
+				
+					if (ENVIRONMENT !== 'testing' && is_cli())
+					{
+						CLI::write("Registered {$task->name} from {$class}", 'green');
+					}
+				}
+			}
+		}
+		
+		return $count;
+	}
+}
