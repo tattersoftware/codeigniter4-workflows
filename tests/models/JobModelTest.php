53863d776b504bbb9023cf0385eb2620659c1058
MGatner <mgatner@icloud.com>
2021-04-01 17:20:40 +0000

    Add JobsTrait tests

---
+++ tests/models/JobModelTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Myth\Auth\Test\Fakers\UserFaker;
+use Tatter\Users\UserEntity;
+use Tatter\Workflows\Entities\Joblog;
+use Tatter\Workflows\Models\JobModel;
+use Tests\Support\DatabaseTestCase;
+
+class JobModelTest extends DatabaseTestCase
+{
+	protected $migrateOnce = true;
+	protected $seedOnce    = true;
+
+	public function testInsertCreatesJoblog()
+	{
+		$jobId = model(JobModel::class)->insert([
+			'name'        => 'Banana Job',
+			'workflow_id' => 1,
+			'stage_id'    => 42,
+		]);
+
+		$this->seeInDatabase('joblogs', [
+			'job_id'     => $jobId,
+			'stage_from' => null,
+			'stage_to'   => 42,
+		]);
+	}
+
+	public function testUpdateCreatesJoblog()
+	{
+		$job = fake(JobModel::class);
+
+		model(JobModel::class)->update($job->id, [
+			'stage_id' => $job->stage_id + 1,
+		]);
+
+		$this->seeInDatabase('joblogs', [
+			'job_id'     => $job->id,
+			'stage_from' => $job->stage_id,
+			'stage_to'   => $job->stage_id + 1,
+		]);
+	}
+}
