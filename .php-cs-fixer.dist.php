f763a917cdf03b6b74a3ed58d0808e1cc815ea3c
MGatner <mgatner@icloud.com>
2021-09-30 19:11:11 +0000

    Update toolkit

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,17 +1,8 @@
 <?php
 
-/**
- * This file is part of Tatter Workflows.
- *
- * (c) 2021 Tatter Software
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- */
-
+use CodeIgniter\CodingStandard\CodeIgniter4;
 use Nexus\CsConfig\Factory;
 use PhpCsFixer\Finder;
-use Tatter\Tools\Standard;
 
 $finder = Finder::create()
     ->files()
@@ -26,11 +17,9 @@ $options = [
     'cacheFile' => 'build/.php-cs-fixer.cache',
 ];
 
-return Factory::create(new Standard(), $overrides, $options)->forLibrary(
+return Factory::create(new CodeIgniter4(), $overrides, $options)->forLibrary(
     'Tatter Workflows',
     'Tatter Software',
     '',
     2021
 );
-
-return Factory::create(new Standard(), $overrides, $options)->forProjects();
