1dfc0b75a46d648819d8f28710a9a6d8ff33b75d
MGatner <mgatner@icloud.com>
2022-06-29 17:18:58 +0000

    Update Action discovery

---
+++ src/Commands/ActionsList.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of Tatter Workflows.
+ *
+ * (c) 2021 Tatter Software
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Tatter\Workflows\Commands;
+
+use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\CLI;
+use Tatter\Workflows\Factories\ActionFactory;
+
+class ActionsList extends BaseCommand
+{
+    protected $group       = 'Workflows';
+    protected $name        = 'actions:list';
+    protected $description = 'Display available workflow actions';
+    protected $usage       = 'actions:list';
+
+    public function run(array $params = []): void
+    {
+        $actions = ActionFactory::getAllAttributes();
+
+        if ($actions === []) {
+            CLI::write('There are no available actions.', 'yellow');
+        } else {
+            $thead = [
+                'Action ID',
+                'Name',
+                'Role',
+                'Icon',
+                'Category',
+                'Summary',
+                'Class',
+            ];
+            CLI::table($actions, $thead);
+        }
+    }
+}
